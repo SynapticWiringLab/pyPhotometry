e940fd02f0ecbf13e3a367fe98cf991bc398a198
Maxime Maheu <maheu.mp@gmail.com>
2021-02-19 00:16:03 +0100

    Add hardware config to the file as recent version of the software
    Modifications made by Maxime.

--- uPy/photometry_upy.py
+++ uPy/photometry_upy.py
@@ -10,6 +10,20 @@ from machine import Pin
 
 from hardware_config import pins, LED_calibration, ADC_volts_per_division
 
+# Hardware config.
+
+pins = {'analog_1' : 'X11', # Pyboard Pins used for analog and digital signals.
+        'analog_2' : 'X12',
+        'digital_1': 'Y7' ,
+        'digital_2': 'Y8' }
+
+LED_calibration = {'slope' : 38.15,  # Calibration of DAC values against LED currents.
+                   'offset':  6.26}  # DAC_value = offset + slope * LED_current_mA.
+
+ADC_volts_per_division = [0.00010122, 0.00010122] # Analog signal volts per division for signal [1, 2]
+
+# Photometry class.
+
 class Photometry():
 
     def __init__(self):
